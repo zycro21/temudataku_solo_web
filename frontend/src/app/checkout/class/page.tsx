e09d04bf9e43b2b4aa22ba0b3cb820b3a472b6a1
zycro21 <dimaszycro10@gmail.com>
2026-03-10 04:01:44 +0700

    add: 10-03-2026 04:01

--- frontend/src/app/checkout/class/page.tsx
+++ frontend/src/app/checkout/class/page.tsx
@@ -1,13 +1,18 @@
+import { Suspense } from "react";
 import CheckoutClassPage from "@/components/checkout/class/CheckoutClass";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export default function classCheckoutPage() {
-    return (
-      <>
-        <Navbar />
+export default function ClassCheckoutPage() {
+  return (
+    <>
+      <Navbar />
+
+      <Suspense fallback={<div className="p-10 text-center">Loading checkout...</div>}>
         <CheckoutClassPage />
-        <Footer />
-      </>
-  )
+      </Suspense>
+
+      <Footer />
+    </>
+  );
 }
